10cab865d2d7114be9cd0fee207bdeaaa4fd3e87
aljazglavac <aljaz.glavac97@gmail.com>
2018-11-29 17:00:41 +0100

    v1

--- download.sh
+++ download.sh
@@ -14,11 +14,11 @@ WLOG='/home/pi/Documents/wget.log' # log file for wget
 ELOG='/home/pi/Documents/error.log' # default log file for errors
 
 # run wget
-wget -Nc --ftp-user=$USER --ftp-password=$PASS ftp://$HOST/$RLOC --directory-prefix=$DLOC --append-output=$WLOG --no-remove-listing
+wget -Nc --ftp-user=$USER --ftp-password=$PASS ftp://$HOST$RLOC --directory-prefix=$DLOC --append-output=$WLOG --no-remove-listing
 
 # test if wget returned any errors
-rval=$?
-if [ $rval -ne 0 ]; then
-    echo "Error: wget with core: " $rval >> $ELOG
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo "Error: wget with core: " $retval >> $ELOG
 fi
-exit $rval
+exit $retval
