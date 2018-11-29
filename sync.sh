b6f14e255373e0b836cc015773a5e2570078b543
aljazglavac <aljaz.glavac97@gmail.com>
2018-11-29 08:47:36 +0100

    added testing of exit codes

--- sync.sh
+++ sync.sh
@@ -8,11 +8,25 @@ REMOTE='/home/pi/tv/remote.txt'
 LPATH='/home/pi/tv/MEDIA/'
 RPATH='/home/pi/tv/MEDIA/.listing'
 
+ELOG='/home/pi/Documents/error.log' # default log file for errors
+
 # local path listing
 ls -ll $LPATH | tail -n +2 | rev | cut -d ' ' -f 1 | rev > $LOCAL
 
+rval=$?
+if [ $rval -ne 0 ]; then
+    echo "Error: ls with core: " $rval >> $ELOG
+fi
+exit $rval
+
 # remote path listig
 cat $RPATH | tail -n +3 | rev | cut -d ' ' -f 1 | rev > $REMOTE
+
+rval=$?
+if [ $rval -ne 0 ]; then
+    echo "Error: cat with core: " $rval >> $ELOG
+fi
+exit $rval
 
 # find and delete files that are in local storage but not on remote 
 while read -r local <&3; do
@@ -26,3 +40,9 @@ while read -r local <&3; do
 		rm $LPATH$local -f
 	fi
 done 3<$LOCAL
+
+rval=$?
+if [ $rval -ne 0 ]; then
+    echo "Error: synx with core: " $rval >> $ELOG
+fi
+exit $rval
