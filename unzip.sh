93f8591d7b1bb5460c8710576a52ea3fc3a24cef
aljazglavac <aljaz.glavac97@gmail.com>
2018-11-30 22:28:09 +0100

    maybe not even gonna use

--- unzip.sh
+++ unzip.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# paths for local and remote text files otput
-REMOTE='/home/pi/tv/remote.txt'
-
 # path to MEDIA
-MEDIA='/home/pi/tv/MEDIA/'
+MEDIA='/home/pi/tv/MEDIA'
 
 ELOG='/home/pi/Documents/error.log' # default log file for errors
 
 # find zips and unzip them
-while read -r media <&3; do
-	if [ "${media##*.}" = "zip" ]; then
-		unzip $MEDIA$media >> $ELOG
+for entry in $MEDIA/*
+do
+	if [ "${entry##*.}" = "zip" ]; then
+		unzip -o $entry -d $MEDIA >> $ELOG
+		rm $entry -f
 	fi
-done 3<$REMOTE
+done
 
 rval=$?
 if [ $rval -ne 0 ]; then
