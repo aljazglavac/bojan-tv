8545d74d0b3ba1bb60d8ebdfc420053adfdb76c2
Aljaz Glavac <aljaz.glavac@gmail.com>
2018-11-30 06:48:52 +0100

    more file types and feh now timeouts after 5min

--- player.sh
+++ player.sh
@@ -6,23 +6,45 @@ VIDEOPATH="/home/pi/tv/MEDIA"
 # path to log file
 VLOG='/home/pi/Documents/player.log'
 
+# timeout for feh and sleep
+MINUTE=60
+
 # wait so media is downloaded
-sleep 60 
+sleep $MINUTE 
 
 while true; do
         for entry in $VIDEOPATH/*
         do
-		#clear
-		#echo $entry
-		if [ "${entry##*.}" = "mp4" ]; then
-			echo $(date) $(basename "$entry") >> $VLOG 2>&1
-			omxplayer -o hdmi $entry >> $VLOG 2>&1
-		fi
-		if [ "${entry##*.}" = "jpg" ]; then
-			echo $(date) $(basename "$entry") >> $VLOG 2>&1
-			timeout 60 feh -F -Y -Z -x $entry >> $VLOG 2>&1
-		fi
+				if [ "${entry##*.}" = "mp4" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					omxplayer -o hdmi $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "mov" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					omxplayer -o hdmi $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "jpg" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					timeout $MINUTE*5 feh -F -Y -Z -x $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "png" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					timeout $MINUTE*5 feh -F -Y -Z -x $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "jpeg" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					timeout $MINUTE*5 feh -F -Y -Z -x $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "JPG" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					timeout $MINUTE*5 feh -F -Y -Z -x $entry >> $VLOG 2>&1
+				fi
+				if [ "${entry##*.}" = "PNG" ]; then
+					echo $(date) $(basename "$entry") >> $VLOG 2>&1
+					timeout $MINUTE*5 feh -F -Y -Z -x $entry >> $VLOG 2>&1
+				fi
         done
 done
+
 exit 0;
 
