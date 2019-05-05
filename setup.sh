42c8b3f7b2176f7ca2a675d7cceec4254b3c031d
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-05-05 13:36:17 +0200

    sudo everything

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo -s
-
-apt-get install ftp feh omxplayer -y
-curl -sL https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/installer.sh | bash -
-echo '\nnetwork={\n\tssid="KRAS"\n\tpsk="inetkras"\n}\n' >> /etc/wpa_supplicant/wpa_supplicant.conf
-crontab mycron
-mkdir -p /home/pi/tv/MEDIA/
+sudo apt-get install ftp feh omxplayer -y
+sudo curl -sL https://raw.githubusercontent.com/AndrewFromMelbourne/raspi2png/master/installer.sh | bash -
+sudo echo '\nnetwork={\n\tssid="KRAS"\n\tpsk="inetkras"\n}\n' >> sudo /etc/wpa_supplicant/wpa_supplicant.conf
+sudo crontab /home/pi/bojan-tv/mycron
+sudo mkdir -p /home/pi/tv/MEDIA/
