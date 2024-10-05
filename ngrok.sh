f8541307f0ba1630ea598b1c09d50c485a24a049
Aljaž Glavač <aljaz.glavac97@gmail.com>
2024-10-05 07:48:26 +0200

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,2 @@
+sudo tar -xvzf ~/Downloads/https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz -C /usr/local/bin
+ngrok config add-authtoken QECjT3sbKShNyvL3FUHT_7pGH6KF38t9h5Vuk2ttbC
