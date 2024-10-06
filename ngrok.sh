339dfdee7fad7c5a6c855920f7b668e1cb165998
Aljaž Glavač <aljaz.glavac97@gmail.com>
2024-10-06 10:38:54 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,2 +1,7 @@
-sudo tar -xvzf ~/Downloads/https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz -C /usr/local/bin
-ngrok config add-authtoken QECjT3sbKShNyvL3FUHT_7pGH6KF38t9h5Vuk2ttbC
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+ ngrok config add-authtoken QECjT3sbKShNyvL3FUHT_7pGH6KF38t9h5Vuk2ttbC
